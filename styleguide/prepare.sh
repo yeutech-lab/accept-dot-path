cdcba103d920564d25258e677da5258bc3530e21
Dimitri Alexander Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2018-06-24 21:50:58 +0700

    fix(ci): build

--- styleguide/prepare.sh
+++ styleguide/prepare.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-npx rollup-umd-scripts doc variable \
+npx @yeutech/rollup-umd-scripts doc variable \
   PACKAGE_NAME=${PACKAGE_NAME} \
-  PACKAGE_PEERS="$(npx rollup-umd-scripts peer npm-install-cmd)" \
+  PACKAGE_PEERS="$(npx @yeutech/rollup-umd-scripts peer npm-install-cmd)" \
   PACKAGE_VERSION=${PACKAGE_VERSION} \
   NODE_VERSION=${NODE_VERSION} \
   NPM_VERSION=${NPM_VERSION} \
@@ -10,4 +10,4 @@ npx rollup-umd-scripts doc variable \
   CI_PROJECT_URL=${CI_PROJECT_URL} \
   CI_PROJECT_NAMESPACE=${CI_PROJECT_NAMESPACE} \
   CI_PROJECT_NAME=${CI_PROJECT_NAME} \
-  IMG_SHIELD_PUBLISHING=$(npx rollup-umd-scripts publish status --badge)
+  IMG_SHIELD_PUBLISHING=$(npx @yeutech/rollup-umd-scripts publish status --badge)
